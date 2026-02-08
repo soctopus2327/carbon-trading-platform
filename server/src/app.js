b2476fbe03cc4c256654e976674beb4a281b582b
soctopus2327 <shriyas0520@gmail.com>
2026-02-09 00:08:35 +0530

    initial project setup

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require('express')
+const cors = require('cors')
+const connectDB = require('./config/db')
+
+const app = express()
+
+connectDB()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('API Running')
+})
+
+module.exports = app
+
+
